fecf6ccac4006dbd8bd06e001e5e96ebf2504863
Automaton <nayben@gmail.com>
2019-02-20 13:33:46 +0000

    Deploy to GitHub pages [ci skip]

---
+++ precache-manifest.fd39f70494b07bd990e4a01a56791f3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0f5f4504980ab5b6fd5",
+    "url": "/tasker/static/css/main.2c80254c.chunk.css"
+  },
+  {
+    "revision": "d0f5f4504980ab5b6fd5",
+    "url": "/tasker/static/js/main.d0f5f450.chunk.js"
+  },
+  {
+    "revision": "bf8649e06d0506145610",
+    "url": "/tasker/static/js/1.bf8649e0.chunk.js"
+  },
+  {
+    "revision": "902719eeabb12797edba",
+    "url": "/tasker/static/js/runtime~main.902719ee.js"
+  },
+  {
+    "revision": "8f4a630364c3f03a2187aa1b356940dd",
+    "url": "/tasker/index.html"
+  }
+];
