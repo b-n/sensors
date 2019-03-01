a3cb7ae6f1ec14b18b5ca99076ca15c273d83805
Ben Naylor <nayben@gmail.com>
2019-03-01 08:22:38 +0100

    feat(web): cast auto redirect on queryparam (gh pages support)

--- web/src/App.js
+++ web/src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Home from './pages/Home'
 import CastReciever from './pages/CastReciever'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import queryString from 'query-string'
 
 const App = (props) => {
+
   return(
     <Router basename={process.env.NODE_ENV === 'development' ? '' : '/sensors'}>
       <div>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={({history, location}) => {
+          if (queryString.parse(location.search).reciever !== undefined) {
+            history.push('/reciever');
+            return null;
+          } 
+          return (<Home />)
+        }} />
         <Route path="/reciever" component={CastReciever} />
       </div>
     </Router>
